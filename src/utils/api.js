342385485d95eab04d5beb8b9c3f847a5da4f844
worldjones <worldjones@gmail.com>
2021-11-24 03:18:42 +0100

    fixed

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API_MAIN = axios.create({
-	baseURL: "http://localhost:8080/jpareststarter/api",
+	baseURL: "http://localhost:8080/jpareststarter/api/",
 	headers: {
 		"Content-type": "application/json",
 		Accept: "application/json",
